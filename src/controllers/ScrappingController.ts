a8d0a8ee42596f2c6a7f55fc6f19eac98be23342
Guy Eisenbach <guyeise5@gmail.com>
2021-07-22 21:58:07 +0300

    Aho-Corasick

--- src/controllers/ScrappingController.ts
+++ src/controllers/ScrappingController.ts
@@ -1,8 +1,31 @@
 import {AbstractController} from "./AbstractController";
 import {model} from "../models/BreakingNews"
+import {Router} from "express";
+const AhoCorasick =require('ahocorasick');
 
 export class ScrappingController extends AbstractController {
     public constructor() {
         super("BreakingNews", model);
+    }
+
+    createRouter(): Router {
+        let router = super.createRouter();
+        router.get("/BreakingNews/q/:words", this.findBy)
+        return router
+    }
+
+    public findBy = async (req: any, res: any) => {
+        let words = req.params["words"].split(',').map((s: String) => s.toLowerCase())
+        await model.find((err: any, documents: Document[]) => {
+            if (err) {
+                res.status(400).json(err)
+            } else {
+                const ac = new AhoCorasick(words);
+                // @ts-ignore
+                res.status(200).json(documents.filter(d => ac.search(d.toObject().content.toLowerCase()).length > 0))
+
+            }
+        })
+
     }
 }
