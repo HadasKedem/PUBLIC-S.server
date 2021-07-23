bc7bb477d6ba635efe675b79c2959892b1fdb57d
Guy Eisenbach <guyeise5@gmail.com>
2021-07-23 11:29:43 +0300

    MapReduce - average words in Article

--- src/controllers/ArticleController.ts
+++ src/controllers/ArticleController.ts
@@ -4,16 +4,17 @@ import {Router} from "express";
 
 export class ArticleController extends AbstractController {
     public constructor() {
-        super("Article", model )
+        super("Article", model)
     }
 
-    public createRouter():Router {
-        let router:Router = super.createRouter();
+    public createRouter(): Router {
+        let router: Router = super.createRouter();
         router.get("/Article/group/byField", this.articleByField)
+        router.get("/Article/q/averageWords", this.averageWords)
         return router;
     }
 
-    public articleByField = async (req:any , res: any) => {
+    public articleByField = async (req: any, res: any) => {
         let aggregator = [{
             $group: {
                 _id: "$field",
@@ -22,7 +23,7 @@ export class ArticleController extends AbstractController {
                 }
             }
         }]
-        await this.model.aggregate(aggregator, (err:any, answer:any) => {
+        await this.model.aggregate(aggregator, (err: any, answer: any) => {
             if (err) {
                 return res.status(400).json({error: err.error})
             } else {
@@ -30,6 +31,32 @@ export class ArticleController extends AbstractController {
             }
         });
 
+    }
+
+    public averageWords = async (req: any, res: any) => {
+        await this.model.find((err: any, documents: Document[]) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                this.model.count((err: any, count: number) => {
+                    if (err) {
+                        res.status(400).json(err)
+                    } else {
+                        let average = -1
+
+                        if (count != 0) {
+                            //@ts-ignore
+                            average = documents.map(d => d.toObject().content.length).reduce((a, b) => a + b) / count
+                        }
+
+                        res.status(200).json({"average": average})
+                    }
+
+                });
+
+
+            }
+        })
     }
 
 }
