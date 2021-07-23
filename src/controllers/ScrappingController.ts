36f24cfc207f9dab3c0043e3ae07a6624653b7f4
Guy Eisenbach <guyeise5@gmail.com>
2021-07-23 17:21:48 +0300

    No one is allowed to modify Scrapping

--- src/controllers/ScrappingController.ts
+++ src/controllers/ScrappingController.ts
@@ -5,7 +5,7 @@ const AhoCorasick =require('ahocorasick');
 
 export class ScrappingController extends AbstractController {
     public constructor() {
-        super("BreakingNews", model, user => user.isAdmin);
+        super("BreakingNews", model, () => false);
     }
 
     createRouter(): Router {
