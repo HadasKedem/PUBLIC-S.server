3e52caa0ffbaf07e0c68384a34ac74730f13f5dc
Guy Eisenbach <guyeise5@gmail.com>
2021-07-24 18:21:48 +0300

    Admins can edit counties

--- src/controllers/CountryController.ts
+++ src/controllers/CountryController.ts
@@ -1,18 +1,8 @@
 import {AbstractController} from "./AbstractController";
 import {model} from "../models/Country";
-import {Router} from "express";
 
 export class CountryController extends AbstractController {
     public constructor() {
-        super("Countries", model, () => false);
-    }
-
-    createRouter(): Router {
-        let rootPath = "/Countries"
-        let router = Router()
-        router.get(rootPath, this.getAll)
-        router.get(rootPath+"/page/:page", this.getByPage)
-        router.get(rootPath + "/:_id", this.getOne)
-        return router
+        super("Countries", model, user => user.isAdmin);
     }
 }
